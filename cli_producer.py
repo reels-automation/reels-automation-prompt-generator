3281151ca92d2da32af3f9b2782a1629a9c89fa5
darthpedroo <pedrovillarino06@gmail.com>
2024-12-04 01:25:44 -0300

    linter and format fixes

--- cli_producer.py
+++ cli_producer.py
@@ -1,28 +1,23 @@
-import time
-import logging
-import json
+
 from quixstreams import Application
 from tema.tema_director import TemaDirector
 
+
 def main():
-    
-    app_producer = Application(
-                            broker_address="localhost:9092",
-                            loglevel = "DEBUG"
-                        )
-    
+
+    app_producer = Application(broker_address="localhost:9092", loglevel="DEBUG")
+
     tema_director = TemaDirector()
-    
+
     with app_producer.get_producer() as producer:
         while True:
             tema = input("Ingresa un tema \n")
             personaje = input("Ingresa un personaje \n")
-            topic = tema_director.build_tema_con_personaje_sin_author(tema,personaje)
+            topic = tema_director.build_tema_con_personaje_sin_author(tema, personaje)
             producer.produce(
-                topic = "temas",
-                key="temas_input_humano",
-                value = str(topic.__dict__)
+                topic="temas", key="temas_input_humano", value=str(topic.__dict__)
             )
+
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
 import logging
-import json
 import ast
 from quixstreams import Application
 from script_video.script_video_personaje_generator import ScriptVideoPersonajeGenerator
@@ -11,48 +9,51 @@ def main():
     tema_director = TemaDirector()
     app_consumer = Application(
         broker_address="localhost:9092",
-        loglevel = "DEBUG",
-        consumer_group = "temas_reader",
+        loglevel="DEBUG",
+        consumer_group="temas_reader",
         auto_offset_reset="latest",
     )
-    
+
     with app_consumer.get_consumer() as consumer:
         consumer.subscribe(["temas"])
-        
+
         while True:
-                msg = consumer.poll(1)
-                if msg is None:
-                    print("Waiting...")
-                elif msg.error() is not None:
-                    raise Exception(msg.error())
-                else:
-                    print(msg.value())
-                    key = msg.key().decode("utf8")
-                    msg_value = msg.value()
-                    offset = msg.offset()
-                    print(f"{offset} {key} {msg_value}")
-                    consumer.store_offsets(msg)
-                    app_producer = Application(
-                        broker_address="localhost:9092",
-                        loglevel = "DEBUG"
-                    )    
-                    with app_producer.get_producer() as producer:
-                            
-                            msg_value = ast.literal_eval(msg_value.decode("utf-8"))
-                            print(msg_value)
-                            print("Started producing...")
-                            
-                            topic = tema_director.build_tema_con_personaje_sin_author(msg_value["tema"],msg_value["personaje"])
-                            script_generator = ScriptVideoPersonajeGenerator()
-                            prompt = script_generator.crear_prompt(topic)
-                            script_video = script_generator.generar_script_video(prompt, topic.__dict__)
-                            producer.produce(
-                                topic = "scripts_video",
-                                key = "Ai Scripts",
-                                value = str(script_video.__dict__)
-                            )
-                            logging.info("Produced. Sleeping..")
-            
+            msg = consumer.poll(1)
+            if msg is None:
+                print("Waiting...")
+            elif msg.error() is not None:
+                raise ValueError(msg.error())
+            else:
+                print(msg.value())
+                key = msg.key().decode("utf8")
+                msg_value = msg.value()
+                offset = msg.offset()
+                print(f"{offset} {key} {msg_value}")
+                consumer.store_offsets(msg)
+                app_producer = Application(
+                    broker_address="localhost:9092", loglevel="DEBUG"
+                )
+                with app_producer.get_producer() as producer:
+
+                    msg_value = ast.literal_eval(msg_value.decode("utf-8"))
+                    print(msg_value)
+                    print("Started producing...")
+
+                    topic = tema_director.build_tema_con_personaje_sin_author(
+                        msg_value["tema"], msg_value["personaje"]
+                    )
+                    script_generator = ScriptVideoPersonajeGenerator()
+                    prompt = script_generator.crear_prompt(topic)
+                    script_video = script_generator.generar_script_video(
+                        prompt, topic.__dict__
+                    )
+                    producer.produce(
+                        topic="scripts_video",
+                        key="Ai Scripts",
+                        value=str(script_video.__dict__),
+                    )
+                    logging.info("Produced. Sleeping..")
+
 
 if __name__ == "__main__":
     logging.basicConfig(level="DEBUG")
